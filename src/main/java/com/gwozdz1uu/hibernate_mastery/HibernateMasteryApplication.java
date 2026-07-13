5e0bdadc90c0be1309429964fa748ee9d44a861f
Gwozdz1uu <gwozdeckidaniel@gmail.com>
2026-07-13 18:56:07 +0200

    initial commit

---
+++ src/main/java/com/gwozdz1uu/hibernate_mastery/HibernateMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.gwozdz1uu.hibernate_mastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateMasteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwozdz1uu/hibernate_mastery/HibernateMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwozdz1uu.hibernate_mastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
