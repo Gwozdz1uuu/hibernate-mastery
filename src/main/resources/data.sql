3602942eced35c03cb181e65703e766e4371d829
Gwozdz1uu <gwozdeckidaniel@gmail.com>
2026-07-13 21:26:10 +0200

    Preparing db data initializations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO training_type (training_type_name) VALUES ('Yoga');
+INSERT INTO training_type (training_type_name) VALUES ('Zumba');
+INSERT INTO training_type (training_type_name) VALUES ('Stretching');
+INSERT INTO training_type (training_type_name) VALUES ('Resistance');
+INSERT INTO training_type (training_type_name) VALUES ('Cardio');
